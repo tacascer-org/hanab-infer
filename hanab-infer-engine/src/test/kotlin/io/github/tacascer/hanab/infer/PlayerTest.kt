ddf75dd97cea4f45b123105028b27b8e9a404d6f
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-06-02 20:07:08 -0400

    test(player): use `shouldContain` (#10)

--- hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/infer/PlayerTest.kt
+++ hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/infer/PlayerTest.kt
@@ -1,20 +1,21 @@
 package io.github.tacascer.hanab.infer
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.property.checkAll
 
 class PlayerTest : FunSpec({
     test("when a player is given a number clue, the player should have that clue in their numberClues list") {
         checkAll(randomPlayer, randomNumberClue) { player, clue ->
             val playerWithClue = player.addClue(clue)
-            playerWithClue.numberClues.contains(clue)
+            playerWithClue.numberClues shouldContain clue
         }
     }
 
     test("when a player is given a color clue, the player should have that clue in their colorClues list") {
         checkAll(randomPlayer, randomColorClue) { player, clue ->
             val playerWithClue = player.addClue(clue)
-            playerWithClue.colorClues.contains(clue)
+            playerWithClue.colorClues shouldContain clue
         }
     }
 })
