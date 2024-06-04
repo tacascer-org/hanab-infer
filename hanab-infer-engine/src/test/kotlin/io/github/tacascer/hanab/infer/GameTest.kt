67e9720d9c5d2dc7c9b227df92261bf35cb4df24
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-06-04 00:19:55 -0400

    test(Game): Use property tests correctly (#13)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
      - Players are now required to have unique names in the game.
    
    - **Refactor**
      - Improved internal mapping structure for player names.
      - Enhanced test generation for game scenarios using random data.
    
    - **Bug Fixes**
    - Fixed an issue where player names could be duplicated by ensuring
    names are UUID-based.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/infer/GameTest.kt
+++ hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/infer/GameTest.kt
@@ -1,6 +1,5 @@
 package io.github.tacascer.hanab.infer
 
-import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldContainExactly
@@ -8,14 +7,9 @@ import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.next
+import io.kotest.property.checkAll
 
 class GameTest : FunSpec({
-    test("can create game") {
-        shouldNotThrowAny {
-            randomGame.next()
-        }
-    }
-
     test("can't create game with less than 2 players") {
         val exception =
             shouldThrow<IllegalArgumentException> { Game(Arb.list(randomPlayer(0), 1..1).next()) }
@@ -32,31 +26,33 @@ class GameTest : FunSpec({
 
     context("clued past") {
         test("when a clue is given, if the clue is not for the next player, then infer that the next player has been clued past") {
-            val game = randomGame(4).next()
-            val players = game.players
-            val clue =
-                randomNumberClue.next().copy(
-                    from = players[0].name,
-                    to = players[2].name,
-                )
+            checkAll(randomGame(4)) { game ->
+                val players = game.players
+                val clue =
+                    randomNumberClue.next().copy(
+                        from = players[0].name,
+                        to = players[2].name,
+                    )
 
-            val gameWithClue = game.addClue(clue)
+                val gameWithClue = game.addClue(clue)
 
-            gameWithClue.inferences[players[1]].shouldContainExactly(Inferences.CLUED_PAST)
+                gameWithClue.inferences[players[1]].shouldContainExactly(Inferences.CLUED_PAST)
+            }
         }
 
         test("when a clue is given, if the clue is for the next player, then don't infer that the next player has been clued past") {
-            val game = randomGame(4).next()
-            val players = game.players
-            val clue =
-                randomNumberClue.next().copy(
-                    from = players[0].name,
-                    to = players[1].name,
-                )
+            checkAll(randomGame(4)) { game ->
+                val players = game.players
+                val clue =
+                    randomNumberClue.next().copy(
+                        from = players[0].name,
+                        to = players[1].name,
+                    )
 
-            val gameWithClue = game.addClue(clue)
+                val gameWithClue = game.addClue(clue)
 
-            gameWithClue.inferences[players[1]] shouldBe null
+                gameWithClue.inferences[players[1]] shouldBe null
+            }
         }
     }
 })
