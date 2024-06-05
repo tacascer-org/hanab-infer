946fa16bfc2d30521b295cf5489a713fe52783b2
tacascer <trandangtrithanh2000@gmail.com>
2024-06-05 00:35:46 -0400

    test(game): add more tests for game

--- hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/engine/GameTest.kt
+++ hanab-infer-engine/src/test/kotlin/io/github/tacascer/hanab/engine/GameTest.kt
@@ -20,6 +20,43 @@ class GameTest : FunSpec({
             shouldThrow<IllegalArgumentException> {
                 Game(Arb.list(randomPlayer(), 6..10).next())
             }
+
         exception.message shouldBe "Hanabi is a game for 2-5 players"
+    }
+
+    test("given a game with 2 to 3 players whose hand size is not 5, when create game, then throw IllegalArgumentException") {
+        val exception =
+            shouldThrow<IllegalArgumentException> {
+                Game(
+                    Arb.list(randomPlayer(hand = Arb.list(randomCard(), 0..4).next()), 2..3).next(),
+                )
+            }
+
+        exception.message shouldBe "Players in a 2-3 player game must have 5 cards"
+    }
+
+    test("given a game with 4 to 5 players whose hand size is not 4, when create game, then throw IllegalArgumentException") {
+        val exception =
+            shouldThrow<IllegalArgumentException> {
+                Game(
+                    Arb.list(randomPlayer(hand = Arb.list(randomCard(), 0..3).next()), 4..5).next(),
+                )
+            }
+
+        exception.message shouldBe "Players in a 4-5 player game must have 4 cards"
+    }
+
+    test("given a game with duplicate player names, when create game, then throw IllegalArgumentException") {
+        val exception =
+            shouldThrow<IllegalArgumentException> {
+                Game(
+                    listOf(
+                        Player("Alice", Arb.list(randomCard(), 5..5).next()),
+                        Player("Alice", Arb.list(randomCard(), 5..5).next()),
+                    ),
+                )
+            }
+
+        exception.message shouldBe "Players must have unique names"
     }
 })
